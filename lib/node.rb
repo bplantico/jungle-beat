300f95c93878c8c862a2b4938443c5d59a200631
Brian Plantico <bplantico@gmail.com>
2019-06-17 09:36:00 -0600

    Initial commit

---
+++ lib/node.rb
@@ -0,0 +1,7 @@
+class Node
+
+  def initialize(data)
+    @data = data
+  end
+  
+end
---
+++ test/node_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/emoji'
+require './lib/node'
+
+class NodeTest < Minitest::Test
+  def setup
+    @node = Node.new("plop")
+  end
+
+  def test_it_exists
+    assert_instance_of Node, @node
+  end
+end
