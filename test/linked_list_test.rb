cb03128d269365aadb342004b630caaf3dd96828
Brian Plantico <bplantico@gmail.com>
2019-06-17 09:44:54 -0600

    Adds skeleton LinkedList class and accompanying test file

---
+++ test/linked_list_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/emoji'
+require './lib/node'
+require './lib/linked_list'
+
+class LinkedListTest < Minitest::Test
+  def setup
+    @list = LinkedList.new
+  end
+
+  def test_it_exists
+    assert_instance_of LinkedList, @list
+  end
+end
