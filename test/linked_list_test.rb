1dd3e656cf595f223997181ef9d45f26ffe6e539
Brian Plantico <bplantico@gmail.com>
2019-06-18 12:18:46 -0600

    Removes white space from linked_list_test file

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -8,7 +8,7 @@ class LinkedListTest < Minitest::Test
   def setup
     @list = LinkedList.new
   end
-  
+
   def test_it_exists
     assert_instance_of LinkedList, @list
   end
@@ -19,37 +19,28 @@ class LinkedListTest < Minitest::Test
 
   def test_it_can_append_to_list
     assert_equal "doop", @list.append("doop")
-
     assert_equal "doop", @list.head.data
     assert_nil @list.head.next_node
   end
 
   def test_it_keeps_count_of_nodes
     assert_equal 0, @list.count
-
     @list.append("doop")
-
     assert_equal 1, @list.count
   end
 
   def test_it_can_print_data_to_string
     assert_equal "", @list.to_string
-
     @list.append("doop")
     assert_equal "doop", @list.to_string
   end
 
   def test_it_appends_multiple_nodes
     @list.append("doop")
-
     assert_nil @list.head.next_node
-
     @list.append("deep")
-
     assert_instance_of Node, @list.head.next_node
-
     @list.append("BBBEEEEPP")
-
     assert_nil @list.head.next_node.next_node.next_node
   end
 
