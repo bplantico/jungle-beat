04c699fa65662f004f3869550cfe26a95ac73b3e
Brian Plantico <bplantico@gmail.com>
2019-06-18 22:23:47 -0600

    Adds LinkedList instantiation to JungleBeat class

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,3 +1,9 @@
 class JungleBeat
-  
+
+  attr_reader :list
+
+  def initialize
+    @list = LinkedList.new
+  end
+
 end
--- test/jungle_beat_test.rb
+++ test/jungle_beat_test.rb
@@ -10,4 +10,9 @@ class JungleBeatTest < Minitest::Test
     assert_instance_of JungleBeat, @jb
   end
 
+  def test_it_initializes_with_a_linked_list
+    assert_instance_of LinkedList, @jb.list
+    assert_nil @jb.list.head
+  end
+
 end
